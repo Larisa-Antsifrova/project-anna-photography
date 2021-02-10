d2dd49da075af058bcd0a1e807bac3e45951db96
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-11 00:10:19 +0200

    Added Materialize light boxes to images.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
 // Initializing sidenav
 const sidenav = document.querySelector(".sidenav");
 const sidenavInstance = M.Sidenav.init(sidenav);
+
+const imgBoxes = document.querySelectorAll(".materialboxed");
+const imgBoxInstances = M.Materialbox.init(imgBoxes);
