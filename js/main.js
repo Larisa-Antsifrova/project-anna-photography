87578927c839f74b2796328e28726a098791c40d
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-02-11 22:41:53 +0200

    Added tabs.

--- js/main.js
+++ js/main.js
@@ -7,3 +7,6 @@ const imgBoxInstances = M.Materialbox.init(imgBoxes);
 
 const parallaxes = document.querySelectorAll(".parallax");
 const parallaxInstances = M.Parallax.init(parallaxes);
+
+const tabs = document.querySelectorAll(".tabs");
+const tabInstances = M.Tabs.init(tabs);
