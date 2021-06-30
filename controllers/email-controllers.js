54ad3bb3ccbefd63d3f2a8050b7747dc99bf9ec5
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-07-01 01:13:31 +0300

    Activated email service.

--- controllers/email-controllers.js
+++ controllers/email-controllers.js
@@ -4,7 +4,7 @@ const sendEmail = async (req, res, next) => {
   try {
     const emailBody = EmailService.createEmailBody(req.body);
 
-    // await EmailService.sendEmail(emailBody);
+    await EmailService.sendEmail(emailBody);
 
     return res.json({ message: "Email sent" });
   } catch (error) {
